0f85ac34aec0d2be9686b610167d38cd4eabb1b8
Stephen Tan <sttan@ucsd.edu>
2020-10-22 14:43:02 -0700

    Added Collection View for superhero related movies

---
+++ Flix/cells/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Stephen Tan on 10/22/20.
+//  Copyright © 2020 Stephen Tan. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
